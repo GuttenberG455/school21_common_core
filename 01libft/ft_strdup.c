784814a3644b19f1bdc96fdeaa42fd3d448048f7
Evgeniy GuttenberG <guttenberg455@gmail.com>
2021-10-18 12:39:12 +0300

    010_libft__10_strdup_fixed

--- 01libft/ft_strdup.c
+++ 01libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: majacqua <majacqua@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/05 16:27:50 by majacqua          #+#    #+#             */
-/*   Updated: 2021/10/13 17:14:29 by majacqua         ###   ########.fr       */
+/*   Updated: 2021/10/18 12:37:44 by majacqua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,8 @@ char	*ft_strdup(char *src)
 	char	*str;
 	int		i;
 
-	i = 0;
-	while (src[i])
-		i++;
-	str = (char *) malloc(i + 1);
+	i = ft_strlen(src);
+	str = (char *) malloc(sizeof(char) * i + 1);
 	if (!str)
 		return (0);
 	i = 0;
