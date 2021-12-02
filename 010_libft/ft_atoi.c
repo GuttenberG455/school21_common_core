74dca0cc5c03dd2911b55604d6f65fa9524053dd
Evgeniy GuttenberG <guttenberg455@gmail.com>
2021-12-02 11:26:37 +0300

    010_libft__03_Complete__New_Atoi

--- 010_libft/ft_atoi.c
+++ 010_libft/ft_atoi.c
@@ -1,34 +1,40 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: majacqua <majacqua@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/05 10:01:02 by majacqua          #+#    #+#             */
-/*   Updated: 2021/10/09 15:57:23 by majacqua         ###   ########.fr       */
+/*   Created: 2021/12/02 11:17:08 by majacqua          #+#    #+#             */
+/*   Updated: 2021/12/02 11:25:02 by majacqua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_atoi(const char *str)
+long long	ft_atoi(const char *str)
 {
-	int	i;
-	int	num;
-	int	neg;
+	long long	i;
+	long long	rtn;
+	char		is_neg;
 
-	neg = 1;
-	num = 0;
 	i = 0;
+	rtn = 0;
+	is_neg = '+';
 	while (str[i] && (str[i] == ' ' || ((str[i] >= 9) && (str[i] <= 13))))
 		i++;
 	if (str[i] == '-')
-		neg = -1;
-	if (str[i] == '-' || str[i] == '+')
-		i++;
-	while (str[i] && str[i] >= '0' && str[i] <= '9')
+		is_neg = str[i++];
+	if (str[i] == '+' && is_neg == '-')
+		return (0);
+	i += (str[i] == '+');
+	while (str[i] >= '0' && str[i] <= '9')
 	{
-		num = num * 10 + (str[i] - '0');
+		rtn *= 10;
+		rtn += str[i] - '0';
 		i++;
 	}
-	return (num * neg);
+	if (rtn < 0)
+		return ((is_neg != '-') * -1);
+	if (is_neg == '-')
+		rtn *= -1;
+	return (rtn);
 }
