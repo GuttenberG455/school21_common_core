05b1987c2dd5e2f0fe90f3da77ec0ddb2b6266bd
Evgeniy GuttenberG <guttenberg455@gmail.com>
2022-02-27 16:55:05 +0300

    042_minishell_2 Errors, builtins

--- 042_minishell/src/const.h
+++ 042_minishell/src/const.h
@@ -6,7 +6,7 @@
 /*   By: majacqua <majacqua@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/19 07:38:49 by rusty             #+#    #+#             */
-/*   Updated: 2022/02/23 15:39:21 by majacqua         ###   ########.fr       */
+/*   Updated: 2022/02/26 18:46:19 by majacqua         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,19 @@
 # define M_ENV	"Environment"
 # define M_PAR	"Parser"
 # define M_CD	"cd"
+# define M_PWD	"pwd"
+# define M_UNS	"unset"
+# define M_EXP	"export"
 
 /* ERRORS */
 # define ERR_NULL_PARAMS	"some parameters are NULL"
-# define ERR_NO_ENV			"env is corrupted"
+# define ERR_NO_ENV			"env file is corrupted"
 # define ERR_MN_ARGS		"too many arguments"
+# define ERR_NO_FILE		"no such file or directory"
+# define ERR_NE_ARGS		"not enough arguments"
+# define ERR_INV_PAR		"invalid parameter name"
+# define ERR_NOT_A_I		"not an identifier"
+# define ERR_NOT_VAL		"not a valid identifier"
 
 /* ASCII */
 # define ASCII_ACK	6
